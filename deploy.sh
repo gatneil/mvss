da92089c5c3af6cf8c4f4290886b0dfc3ab4ccf0
negat <negat@microsoft.com>
2017-06-14 00:19:36 +0000

    fixed string replacement which was failing due to special character

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,6 @@ az storage account create -g ${rgname} -n ${saname} --sku Standard_LRS
 sakey=$(az storage account show-connection-string -g ${rgname} -n ${saname} --output table | grep AccountKey | cut -d '=' -f 5)== # note: very hacky; will likely break in the future since I don't think the order of properties in the connection string is guaranteed
 saskey=$(az storage account generate-sas --expiry $expiry --permissions "acdlpruw" --resource-types "sco" --services "bfqt" --account-name ${rgname} --account-key $sakey | cut -d '"' -f 2) # note: this cut is also probably a tad hacky
 
-cp saTemplate.json sa.parameters.json
-
-sed -i -e "s/SANAME/${saname}/g" sa.parameters.json
-sed -i -e "s/SASASTOKEN/${saskey}/g" sa.parameters.json
+cat saTemplate.json | jq ".parameters.storageAccountName.value = \"${saname}\"" | jq ".parameters.storageAccountSasToken.value = \"${saskey}\"" > sa.parameters.json
 
 az group deployment create -g ${rgname} --template-file azuredeploy.json --parameters @sa.parameters.json
